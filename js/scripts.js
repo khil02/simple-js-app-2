190b7093f7e7fc713edc0fd0b05459da6e833f60
khil02 <120340087+khil02@users.noreply.github.com>
2023-04-09 18:12:01 -0700

    Added for loop
    Added a for loop to cycle through and print out the list, with a conditional to state if height was larger than 6.

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,14 @@ let pokemonlist= [
 ]; 
 //testing that array is working
 console.log(pokemonlist[1]);
-pokemonlist[3] = {name: "Charmander", height: 2, type: ['fire']};  
+pokemonlist[3] = {name: "Charmander", height: 2.00, type: ['fire']};  
 //verifying new object was added to array correctly
 console.log(pokemonlist[3]);
+
+//For let to write out all pokemon on list currently
+for (let i=0; i < pokemonlist.length; i++){
+    document.write('<p>' + pokemonlist[i].name  + '<br>' + "  height: (" + pokemonlist[i].height + ")" )
+    //If statement stating if it is a "big" pokemon
+    let big = pokemonlist[i].height > 6 ? '<br>' + "That's a big one!" : "";
+    document.write(big);
+}
