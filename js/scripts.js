726e575c847fe0f667e950098f2a94ae98e2ea4c
khil02 <120340087+khil02@users.noreply.github.com>
2023-04-13 20:21:06 -0700

    Switched to a IIFE setup

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,39 @@
-let pokemonlist= [
-    {
-        name: "Bulbasuar",
-        height: 2.04,
-        type: ['grass', 'poison']
-    },
-    {
-        name: "Ivyasuar",
-        height: 3.03,
-        type: ['grass', 'poison']
-    },
-    {
-        name: "Venusuar",
-        height: 6.07,
-        type: ['grass', 'poison']
-    }
-]; 
-//testing that array is working
-console.log(pokemonlist[1]);
-pokemonlist[3] = {name: "Charmander", height: 2.00, type: ['fire']};  
-//verifying new object was added to array correctly
-console.log(pokemonlist[3]);
+let pokemonRepository = (function(){
+    let pokemonlist= [
+        {
+            name: "Bulbasuar",
+            height: 2.04,
+            type: ['grass', 'poison']
+        },
+        {
+            name: "Ivyasuar",
+            height: 3.03,
+            type: ['grass', 'poison']
+        },
+        {
+            name: "Venusuar",
+            height: 6.07,
+            type: ['grass', 'poison']
+        }
+]
+function add(pokemon){
+    pokemonlist.push(pokemon);
+}
+
+function getAll(){
+    return pokemonlist;
+}
+return {
+    add: add,
+    getAll: getAll
+    };
+})();
+
+
+pokemonRepository.add({name: "Charmander", height: 2.00, type: ['fire']});  
 
 //For let to write out all pokemon on list currently
-pokemonlist.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
     document.write('<p>' + pokemon.name  + '<br>' + "  height: (" + pokemon.height + ")" )
     //If statement stating if it is a "big" pokemon
     let big = pokemon.height > 6 ? '<br>' + "That's a big one!" : "";
