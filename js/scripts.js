aa4069092628152b76773fac686e261488c9b979
khil02 <120340087+khil02@users.noreply.github.com>
2023-04-25 21:04:31 -0700

    Adds modal
    Adds functions for modal to display information, hide modal, uses the modal to display information about pokemon, and adds a mode to switch which image is being used.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 let pokemonRepository = (function(){
     let pokemonlist= [];
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
+    let modalContainer = document.querySelector("#modal-container");
 
 // adds pokemon to the repository list
 function add(pokemon){
@@ -20,7 +21,9 @@ function add(pokemon){
 function getAll(){
     return pokemonlist;
 }
-// searches list for matching name
+
+/* Not in use currently
+//searches list for matching name
 function search(findMe){
     let result = pokemonlist.filter((e) => e.name == findMe);
     if (result.length !== 0){
@@ -40,6 +43,8 @@ function search(findMe){
         return result;
     }
 }
+*/
+
 //Generates button list
 function addListItem(pokemon){
     let pokedex = document.querySelector('.pokemon-list');
@@ -58,11 +63,8 @@ function addListItem(pokemon){
 //shows the details on screen
 function showDetails(pokemon){
     loadDetails(pokemon).then(function(){
-        console.log(pokemon);
-        //loads the pokemon image
-        let parent = document.getElementById("pokedexPhoto");
-        let imgTag = '<img src="'+pokemon.imageUrl+'">';
-        parent.innerHTML = imgTag;
+        //showModal (title, height, weight, types, img, imgFancy)
+        showModal(pokemon.name, pokemon.height, pokemon.weight, pokemon.types, pokemon.imageUrl, pokemon.imageUrlFancy);
     });
 }
 //Adds a loading GIF at the top of the body
@@ -86,7 +88,8 @@ function loadList(){
     }).then(function(json){
         json.results.forEach(function(item){
             let pokemon = {
-                name: item.name,
+                //capitalizes the first letter of the Pokemon's name
+                name: item.name.charAt(0).toUpperCase()+item.name.substr(1),
                 detailsUrl: item.url
             };
             
@@ -109,16 +112,91 @@ function loadDetails(item) {
         //adds the details to the item
         item.imageUrl = details.sprites.front_default;
         item.height = details.height;
+        item.weight = details.weight;
         item.types = details.types;
+        item.imageUrlFancy = details.sprites.other['official-artwork'].front_default;
+
     }).catch(function(e){
         console.error(e);
     });
 }
 
+//creates Modal to display pokemon information
+function showModal (title, height, weight, types, img, imgFancy){
+
+    modalContainer.innerHTML = " ";
+
+    let modal = document.createElement("div");
+    modal.classList.add("modal");
+
+    let closeModalButton = document.createElement("button");
+    closeModalButton.classList.add("modal-close");
+    closeModalButton.innerText = "Close";
+    closeModalButton.addEventListener("click", hideModal);
+
+    let modalTitle = document.createElement("h2");
+    modalTitle.innerText = title;
+
+    let modalText = document.createElement("p");
+    // Adds Types to an array by pulling them out of the object they are in
+    typesArray = [];
+    for (let i = 0; i < types.length; i++){
+        typesArray.push(" " + types[i].type.name);
+    };
+    // Prints out information about pokemon
+    modalText.innerText = "  Height: " + (height*.1).toFixed(2) + "m" + "\n Weight: " + (weight*.1) + "kg" + "\n Types: " + typesArray;
+
+
+    let modalImage = document.createElement("img");
+    //Checks to see if fancy mode is on
+    modalImage.src = modalContainer.classList.contains("fancy") ? imgFancy : img;
+    modalImage.classList.add("pokemon-image");
+
+    if (fancyOn){
+        modal.classList.add("fancy");
+        closeModalButton.classList.add("fancy");
+    };
+
+    //builds Modal
+    modal.appendChild(closeModalButton);
+    modal.appendChild(modalTitle);
+    modal.appendChild(modalImage);
+    modal.appendChild(modalText);
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add("is-visible");
+    // closes modal if clicked outside of it
+    modalContainer.addEventListener("click", (e) => {
+        let target = e.target;
+        if (target === modalContainer){
+            hideModal();
+        }
+    });
+}
+// hides Modal
+function hideModal(){
+    modalContainer.classList.remove("is-visible");
+}
+let fancyModeButton = document.querySelector(".fancy-mode");
+let fancyOn = false;
+fancyModeButton.addEventListener("click", fancyMode);
+
+//Toggles using sprites or official artwork
+function fancyMode(){
+    modalContainer.classList.toggle("fancy");
+    //toggles on and off
+    fancyOn = !fancyOn;
+    if (fancyOn){
+    fancyModeButton.innerText = "Fancy Mode: On"
+} else {
+    fancyModeButton.innerText = "Fancy Mode: Off"
+};
+}
+
 return {
     add: add,
     getAll: getAll,
-    search: search,
+    //search: search,
     addListItem: addListItem,
     loadList: loadList,
     loadDetails: loadDetails,
@@ -131,11 +209,4 @@ pokemonRepository.loadList().then(function(){
         pokemonRepository.addListItem(pokemon);
     });
 });
-
-     //   document.write('<p>' + pokemon.name  + '<br>' + "  height: (" + pokemon.height + ")"+'</p>' )
-    //     //If statement stating if it is a "big" pokemon
-    //     let big = pokemon.height > 6 ? '<br>' + "That's a big one!" : "";
-    //     document.write(big);
-//});
-
 
