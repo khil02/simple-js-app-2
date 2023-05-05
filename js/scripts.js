f59708de44f4b0b1cc3961f7c3f4359ec04d4faa
khil02 <120340087+khil02@users.noreply.github.com>
2023-05-04 23:43:02 -0700

    Switching to use Bootstrap

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ let pokemonRepository = (function(){
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
     let modalContainer = document.querySelector("#modal-container");
 
+
 // adds pokemon to the repository list
 function add(pokemon){
     //checks to see if it's an object
@@ -22,49 +23,31 @@ function getAll(){
     return pokemonlist;
 }
 
-/* Not in use currently
-//searches list for matching name
-function search(findMe){
-    let result = pokemonlist.filter((e) => e.name == findMe);
-    if (result.length !== 0){
-    console.log(result);
-    
-    //Prints out found information
-    result.forEach(function(pokemon) {
-        document.write('<p>' + pokemon.name  + '<br>' + "  height: (" + pokemon.height + ")" )});    
-    console.log(result);
-    return result;
-    }
-    // Returns as false and prints out Not Found
-    else { 
-        result = false;
-        console.log(result);
-        document.write('<p>' +"Not Found" );
-        return result;
-    }
-}
-*/
-
 //Generates button list
 function addListItem(pokemon){
     let pokedex = document.querySelector('.pokemon-list');
     let listItem = document.createElement('li');
     let button = document.createElement('button');
+
     button.innerText = (pokemon.name);
-    button.classList.add("pokedexButton");
+    button.setAttribute("date-toggle", "modal");
+    button.setAttribute("data-target", "#modal-container");
+    button.setAttribute("aria-pressed","false"); 
+    button.setAttribute("autocomplete","off");
+    button.classList.add("pokedexButton", "list-group-item", "btn", "btn-outline-info");
     listItem.appendChild(button);
     pokedex.appendChild(listItem);
 
     //Listens for button click
-    button.addEventListener('click', function(event){
+    button.addEventListener('click', () => {
         showDetails(pokemon)
-    })
+    });
 }
 //shows the details on screen
 function showDetails(pokemon){
     loadDetails(pokemon).then(function(){
         //showModal (title, height, weight, types, img, imgFancy)
-        showModal(pokemon.name, pokemon.height, pokemon.weight, pokemon.types, pokemon.imageUrl, pokemon.imageUrlFancy);
+        showModal(pokemon);
     });
 }
 //Adds a loading GIF at the top of the body
@@ -92,9 +75,7 @@ function loadList(){
                 name: item.name.charAt(0).toUpperCase()+item.name.substr(1),
                 detailsUrl: item.url
             };
-            
             add(pokemon);
-            
         });
         removeLoadingMessage();
     }).catch(function(e){
@@ -115,6 +96,7 @@ function loadDetails(item) {
         item.weight = details.weight;
         item.types = details.types;
         item.imageUrlFancy = details.sprites.other['official-artwork'].front_default;
+        item.abilities = details.abilities;
 
     }).catch(function(e){
         console.error(e);
@@ -122,64 +104,50 @@ function loadDetails(item) {
 }
 
 //creates Modal to display pokemon information
-function showModal (title, height, weight, types, img, imgFancy){
+function showModal (pokemon){
+    let modalBody = $(".modal-body");
+    let modalTitle = $(".modal-title");
+    //let modalHeader = $(".modal-header");
 
-    modalContainer.innerHTML = " ";
+    modalTitle.empty();
+    modalBody.empty();
 
-    let modal = document.createElement("div");
-    modal.classList.add("modal");
+    //showModal (title, height, weight, types, img, imgFancy)
+    //adding declaring the elements to fill out the modal
+    let pokemonName = $("<h1>" + pokemon.name + "</h1>");
+    let pokemonHeight =$("<p>" + "Height: " + (pokemon.height*.1).toFixed(2) +"</p>");
+    let pokemonWeight =$("<p>" +" Weight: " + (pokemon.weight*.1) + "kg" +"</p>");
 
-    let closeModalButton = document.createElement("button");
-    closeModalButton.classList.add("modal-close");
-    closeModalButton.innerText = "Close";
-    closeModalButton.addEventListener("click", hideModal);
-
-    let modalTitle = document.createElement("h2");
-    modalTitle.innerText = title;
-
-    let modalText = document.createElement("p");
     // Adds Types to an array by pulling them out of the object they are in
-    typesArray = [];
-    for (let i = 0; i < types.length; i++){
-        typesArray.push(" " + types[i].type.name);
-    };
-    // Prints out information about pokemon
-    modalText.innerText = "  Height: " + (height*.1).toFixed(2) + "m" + "\n Weight: " + (weight*.1) + "kg" + "\n Types: " + typesArray;
+    let typesArray = pokemon.types.map((type) => " " + type.type.name); 
+    let pokemonTypes = $("<p>" + "Types:" +typesArray +"</p>");
 
+    // Adds Abilities to an array
+    let abilitiesArray = pokemon.abilities.map((ability) => " " + ability.ability.name);
+    let pokemonAbilities = $("<p>" + "Abilities:" +abilitiesArray +"</p>");
+    let pokemonImg = $("<img class = modal-image style='width:100%'>"); 
+    // MUST adjust modalContainer to correct div
+    pokemonImg.attr("src", /*(modalContainer.classList.contains("fancy") ? pokemon.imageUrlFancy :*/ pokemon.imageUrl/*)*/)
+    
 
-    let modalImage = document.createElement("img");
     //Checks to see if fancy mode is on
-    modalImage.src = modalContainer.classList.contains("fancy") ? imgFancy : img;
-    modalImage.classList.add("pokemon-image");
-
     if (fancyOn){
         modal.classList.add("fancy");
         closeModalButton.classList.add("fancy");
     };
 
     //builds Modal
-    modal.appendChild(closeModalButton);
-    modal.appendChild(modalTitle);
-    modal.appendChild(modalImage);
-    modal.appendChild(modalText);
-    modalContainer.appendChild(modal);
+    modalTitle.append(pokemonName);
+    modalBody.append(pokemonImg);
+    modalBody.append(pokemonHeight);
+    modalBody.append(pokemonWeight);
+    modalBody.append(pokemonTypes);
+    modalBody.append(pokemonAbilities);
+}
 
-    modalContainer.classList.add("is-visible");
-    // closes modal if clicked outside of it
-    modalContainer.addEventListener("click", (e) => {
-        let target = e.target;
-        if (target === modalContainer){
-            hideModal();
-        }
-    });
-}
-// hides Modal
-function hideModal(){
-    modalContainer.classList.remove("is-visible");
-}
 let fancyModeButton = document.querySelector(".fancy-mode");
 let fancyOn = false;
-fancyModeButton.addEventListener("click", fancyMode);
+//fancyModeButton.addEventListener("click", fancyMode);
 
 //Toggles using sprites or official artwork
 function fancyMode(){
